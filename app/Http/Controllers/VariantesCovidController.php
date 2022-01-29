f6354ea38e853379d51cc82e757ec41f5a9848f3
JenniD5 <elizabethduran0105@gmail.com>
2022-01-28 17:08:19 -0700

    practica1

---
+++ app/Http/Controllers/VariantesCovidController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VariantesCovidController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('variantes.index');
+    }
+}
